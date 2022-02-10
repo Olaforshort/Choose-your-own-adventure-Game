a02d683107015f64a49c2c3f7ebc895504ef5034
Ola <32800621+Olaforshort@users.noreply.github.com>
2022-02-10 22:41:40 +0000

    Main body completed

--- main.py
+++ main.py
@@ -2,4 +2,17 @@ print(
     "Welcome to my first choose your own adventure game! \nLet's get acquainted."
 )
 name = input("What's your name? ")
-age = input("How old are you?")
+age = int(input("How old are you?"))
+
+print("Hello", name, "you are", age, "years old.") 
+
+if age >= 18:
+  print("you are old enough!")
+
+  play_consent = input("Do you want to play?")
+  if play_consent.lower == "yes":
+    print("Let's play")
+  else:
+    print("Bye")
+else:
+  print("You aren't old enough to play!")
