c1ca0f9eae4b9279e2c579e3957e59707da1570c
Ola <32800621+Olaforshort@users.noreply.github.com>
2022-02-20 18:34:10 +0000

    name formatting, input formatting

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print(
     "Welcome to my first choose your own adventure game! \nLet's get acquainted."
 )
-name = input("What's your name? ")
+name = input("What's your name? ").capitalize()
 age = int(input("How old are you? "))
 
 print("Hello", name, "you are", age, "years old.") 
@@ -9,17 +9,17 @@ print("Hello", name, "you are", age, "years old.")
 if age >= 18: 
   print("you are old enough!")
 
-  play_consent = input("Do you want to play? ")
-  if play_consent.lower() == "yes" :
+  play_consent = input("Do you want to play? ").lower()
+  if play_consent == "yes" :
     print("Let's play")
   else:
     print("Bye")
 else:
   print("You aren't old enough to play!")
   
-  adult_supervision = input("Is there an adult with you who can supervise?")
+  adult_supervision = input("Is there an adult with you who can supervise?").lower()
 
-  if adult_supervision.lower() == "yes":
+  if adult_supervision == "yes":
     print("Let's play!")
   else:
     ("Bye")
